08016f4ad3a989349d9995dd6ca233c0a348b901
TongToan <cauvongkhuyetbs@gmail.com>
2014-09-03 19:51:26 +0700

    yii2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'mylove133',
+    'charset' => 'utf8',
+];
